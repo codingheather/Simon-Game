8441f91a737153ab1dcbed2873dd6b852cd976bd
Heather Hao <89426050+codingheather@users.noreply.github.com>
2022-06-27 20:46:42 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,70 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 1;
+
+$(document).on('keypress',function() {
+    if(!started) {
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+})
+
+function checkAnswer(currentLevel) {
+  // check the clicked button match gamepattern
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  //  if not match gamepattern, wrong 
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
+
+function nextSequence() {
+   userClickedPattern = [];
+   $("#level-title").text("Level " + level);
+   level++;
+   var randomNumber = Math.floor(Math.random() * 4);
+   var randomChosenColour = buttonColours[randomNumber];
+   gamePattern.push(randomChosenColour);
+   $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+   playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  level = 1;
+  gamePattern = [];
+  started = false;
+}
 
